4b7d0c4b8aa4df31e6b7d7f421ba6327508fc1f3
Justin Roy <justin.roy.tamaela@mendix.com>
2023-10-02 19:45:39 +0200

    added remaining files.

---
+++ src/components/Form.js
@@ -0,0 +1,78 @@
+const Form = () => {
+  const submitHandler = (event) => {
+    event.preventDefault(); // prevents browsers default behavior.
+    // ... Do something upon submission.
+    console.log("SUBMIT");
+  };
+
+  const resetHandler = () => {
+    // .. do something when cancel is clicked.
+    console.log("RESET");
+  };
+
+  const inputChangeHandler = (input, value) => {
+    // .. do something when cancel is clicked.
+    console.log(input, value);
+  };
+
+  return (
+    <form onSubmit={submitHandler} className="form">
+      <div className="input-group">
+        <p>
+          <label htmlFor="current-savings">Current Savings ($)</label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler("current-savings", event.target.value)
+            }
+            type="number"
+            id="current-savings"
+          />
+        </p>
+        <p>
+          <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler("yearly-contribution", event.target.value)
+            }
+            type="number"
+            id="yearly-contribution"
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="expected-return">
+            Expected Interest (%, per year)
+          </label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler("expected-return", event.target.value)
+            }
+            type="number"
+            id="expected-return"
+          />
+        </p>
+        <p>
+          <label htmlFor="duration">Investment Duration (years)</label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler("duration", event.target.value)
+            }
+            type="number"
+            id="duration"
+          />
+        </p>
+      </div>
+      <p className="actions">
+        <button onClick={resetHandler} type="reset" className="buttonAlt">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Calculate
+        </button>
+      </p>
+    </form>
+  );
+};
+
+export default Form;
